064d38fe50048d90fe9d8507f3483bfdaab26086
赵雅国 <1248023357@qq.com>
2022-12-17 22:41:18 +0800

    feat: test init

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,18 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('importtemplate.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('importtemplate.helloWorld', (e) => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from importTemplate!');
+		// vscode.window.showInformationMessage('Hello World from importTemplate!');
+		// vscode.window.showInformationMessage(`copied to the clipboard `);
+		const name = e ? e?.fsPath?.split('/').pop() :
+				vscode.window.activeTextEditor?.document.fileName.split('/').pop();
+        const proc = require('child_process').spawn('pbcopy'); 
+    proc.stdin.write(name);
+        proc.stdin.end();
+    // 复制成功后给一个提示
+        vscode.window.showInformationMessage(`copied '${name}'1111111111111 `);
 	});
 
 	context.subscriptions.push(disposable);
